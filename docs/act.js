e9bae50e210df5c5648273007269ac7631a3d2a2
brageon <153751096+brageon@users.noreply.github.com>
2023-12-27 13:59:39 +0100

    Update act.js

--- docs/act.js
+++ docs/act.js
@@ -1,34 +1,25 @@
-const readline = require('readline');
-function activeToPassive(sentence) {
-  const tokens = sentence.split(' ');
-  const verb = tokens[0];
-  const subject = tokens[1];
-  if (isModalVerb(verb)) {
-    return sentence;
-  } else {
-    const passiveSentence = `The ${subject} was ${verb.lemma} by ${sentence.slice(2)}`;
-    return passiveSentence; }}
-function passiveToActive(sentence) {
-  const tokens = sentence.split(' ');
-  const verb = tokens[2];
-  const subject = tokens[0];
-  const agent = tokens[4];
-  if (isDefiniteArticle(agent[0])) {
-    return sentence;
-  } else {
-    const activeSentence = verb.lemma + ` ${subject} ${sentence.slice(4)}`;
-    return activeSentence; }}
-function isModalVerb(word) {
-  return ['can', 'could', 'may', 'might', 'must', 'should', 'would'].includes(word); }
-function isDefiniteArticle(word) {
-  return ['the', 'this', 'that', 'these', 'those'].includes(word); }
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout });
-rl.question('Enter a sentence: ', (inputSentence) => {
-  const storedSentence = inputSentence;
-  const activeSentence = activeToPassive(storedSentence);
-  console.log('Active sentence:', activeSentence);
-  const passiveSentence = passiveToActive(storedSentence);
-  console.log('Passive sentence:', passiveSentence);
-  rl.close(); });
+const promptButton = document.getElementById('promptButton');
+promptButton.addEventListener('click', async () => {
+  const inputSentence = await prompt('Enter your message:');
+  // Asynchronously process the sentence
+  promptButton.textContent = 'Processing...';
+  let finished = false;
+  let activeSentence;
+  let passiveSentence;
+  const pollInterval = setInterval(() => {
+    if (finished) {
+      clearInterval(pollInterval);
+      promptButton.textContent = 'Active sentence:';
+      console.log('Active sentence:', activeSentence);
+      promptButton.textContent = 'Passive sentence:';
+      console.log('Passive sentence:', passiveSentence);
+    }
+  }, 10); // milliseconds
+  try {
+    const { activeSentence, passiveSentence } = await processSentence(inputSentence);
+    finished = true;
+    this.activeSentence = activeSentence;
+    this.passiveSentence = passiveSentence;
+  } catch (error) {
+    console.error(error);
+}});
